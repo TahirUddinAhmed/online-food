4442775c44b5d933622b7359d46d76e133aa29c9
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2023-11-24 10:05:16 +0530

    home page design Online Food Delivery System

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+    $("#register").click(function() {
+        alert("Work is under construction")
+    })
+   
+    // food owl carousel
+    $("#foods .owl-carousel").owlCarousel({
+        // rtl: true,
+        loop:true,
+        margin:10,
+        nav: true,
+        responsive:{
+            0:{
+                items:1.4
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5.4
+            }
+        }
+    })
+});
