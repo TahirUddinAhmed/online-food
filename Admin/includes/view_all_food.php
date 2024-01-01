eb8c73cca476d414d9e1ad9d70901d8c2035a20a
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-01-01 21:09:59 +0530

    view food table create

--- Admin/includes/view_all_food.php
+++ Admin/includes/view_all_food.php
@@ -1,6 +1,50 @@
 <div id="layoutSidenav_content">
     <main>
-        <div class="container">
-            <h2>View all product</h2> 
+        <div class="container mt-4">
+            <h3 class="mb-4">View all product</h3>
+
+            <div class="border p-2">
+                <table id="datatablesSimple" class="mt-3">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Position</th>
+                            <th>Office</th>
+                            <th>Age</th>
+                            <th>Start date</th>
+                            <th>Salary</th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            <th>Name</th>
+                            <th>Position</th>
+                            <th>Office</th>
+                            <th>Age</th>
+                            <th>Start date</th>
+                            <th>Salary</th>
+                        </tr>
+                    </tfoot>
+                    <tbody>
+                        <tr>
+                            <td>Tiger Nixon</td>
+                            <td>System Architect</td>
+                            <td>Edinburgh</td>
+                            <td>61</td>
+                            <td>2011/04/25</td>
+                            <td>$320,800</td>
+                        </tr>
+                        <tr>
+                            <td>Garrett Winters</td>
+                            <td>Accountant</td>
+                            <td>Tokyo</td>
+                            <td>63</td>
+                            <td>2011/07/25</td>
+                            <td>$170,750</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
         </div>
     </main>
