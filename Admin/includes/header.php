7cef385bbed69bc92dc85e9543bfd19251658e62
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-01-25 09:11:11 +0530

    logout completed

--- Admin/includes/header.php
+++ Admin/includes/header.php
@@ -41,7 +41,7 @@
                     <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                         <li><hr class="dropdown-divider" /></li>
-                        <li><a class="dropdown-item" href="login.html">Logout</a></li>
+                        <li><a class="dropdown-item" href="logout.php">Logout</a></li>
                     </ul>
                 </li>
             </ul>
