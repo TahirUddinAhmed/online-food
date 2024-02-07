0b14cea52ff5d68957e33cfa5587ea87d408d87c
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-02-07 11:39:01 +0530

    top food categories

--- includes/_top_food_items.php
+++ includes/_top_food_items.php
@@ -1,22 +1,47 @@
+<?php
+    //  query 
+    $query = "SELECT * FROM category";
+    $result = mysqli_query($conn, $query);
+
+    if(!$result) {
+        die("QUERY FAILED" . mysqli_error($conn));
+    }
+?>
 <div class="container mt-4">
     <h2 class="text-center">Top Food Items</h2>
     <div class="food-category">
-        <div class="category-items">
-            <div class="cat-img">
-                <img src="./assets/Restaurant/2.jpg" alt="" />
-            </div>
-            <div class="cat-content">
-                <div class="starr">
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star"></span>
+    <?php
+     if(mysqli_num_rows($result)) {
+        while($data=mysqli_fetch_assoc($result)) {
+            $catID = $data['cat_id'];
+            $catName = $data['name'];
+
+            // number of food items added 
+            $FoodQuery = "SELECT * FROM menuitems WHERE categoryID = $catID";
+            $FoodRes = mysqli_query($conn, $FoodQuery);
+
+            if(mysqli_num_rows($FoodRes) > 0) {
+                while($row=mysqli_fetch_assoc($FoodRes)) {
+                    $Food_cat_id = $row['CategoryID'];
+                    $foodImg = $row['food_img'];
+                }
+        ?>
+            <div class="category-items">
+                <div class="cat-img">
+                    <img src="./assets/Restaurant/<?= $foodImg ?>" alt="" />
                 </div>
-                <h4>Veg Burger</h4>
-            </div>
-        </div>
-        <div class="category-items">
+                <div class="cat-content">
+                    <h4><?= $catName ?></h4>
+                </div>
+           </div>
+        <?php
+        
+            }
+        }
+     }
+    ?>
+        
+        <!-- <div class="category-items">
             <div class="cat-img">
                 <img src="./assets/Restaurant/9.jpg" alt="" />
             </div>
@@ -81,14 +106,7 @@
                 <img src="./assets/Restaurant/pasta1.jpg" alt="" />
             </div>
             <div class="cat-content">
-                <div class="starr">
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star"></span>
-                </div>
-                <h4>Dosa</h4>
+                <h4>Dosasss</h4>
             </div>
         </div>
         <div class="category-items">
@@ -120,6 +138,6 @@
                 </div>
                 <h4>Roti Thali</h4>
             </div>
-        </div>
+        </div> -->
     </div>
 </div>
